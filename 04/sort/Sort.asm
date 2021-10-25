b03b5090500fbd1501e2426f1ea01f3d4f322704
rachelassael <58739897+rachelassael@users.noreply.github.com>
2021-10-25 22:38:51 +0300

    Update Sort.asm

--- 04/sort/Sort.asm
+++ 04/sort/Sort.asm
@@ -16,27 +16,132 @@
 // You can implement any sorting algorithm as long as its runtime complexity is 
 // at most C*O(N^2), like bubble-sort. 
 
-// Put your code here.
 
-
-
-//init pointer
+// Put the first address of the array in "startIndex", set the "endIndex", ans set the "isSwapped" to 1 for starting the sort
 @R14
-D = M
-@cur
-M = D
-// init length var
+D=M
+@startIndex
+M=D
 @R15
-D = M
-@len
-M = D
-(LOOP)
-	@swapped
-	M = 0
-	@cur
-	D = A
-	@next
-	M = D
-	(INNERLOOP)
-	// check
-	// swap or goto LOOP
+D=D+M
+@endIndex
+M=D
+@isSwapped
+M=1
+ 
+(firstLoop)
+// Check if there is need for another round of switches, and reset the rellevant variables
+	// Check if the length of the array is 0 or 1, and jump to END if so
+	@R15
+	D=M-1
+	@END
+	D;JLE
+	
+	// Check if the sort ended, and jump to END if so
+	@isSwapped
+	D=M
+	@END
+	D;JEQ
+	
+	// If there is another round to go, set the "isSwapped" to 0
+	@isSwapped
+	M=0
+	// Set the start to the start of the array
+	@R14
+	D=M
+	@startIndex
+	M=D
+	// Jump to the second loop
+	@secondLoop
+	0;JMP
+
+(secondLoop)
+	// If startIndex = endIndex go back to firstLoop, otherwise continue 
+	@startIndex
+	D=M
+	@endIndex
+	D=M-D
+	@firstLoop
+	D;JEQ
+	
+	// Set currentIndex = startIndex
+	@startIndex
+	D=M
+	@currentIndex
+	M=D
+	@thirdLoop
+	0;JMP
+		
+(thirdLoop)
+	// If currentIndex = endIndex-1, jump to GoBackToSec 
+	@endIndex
+	D=M-1
+	@currentIndex
+	D=D-M
+	@GoBackToSec
+	D;JEQ
+	
+	// If currentIndex < endIndex:
+	
+	// Put array[currentIndex] in D
+	@currentIndex
+	A=M
+	D=M
+	
+	// Put [currentIndex+1]-[currentIndex] in D
+	@currentIndex
+	A=M+1
+	D=M-D
+		
+	// If array[currentIndex] < array[currentIndex + 1], jump to Swap
+	@Swap
+	D;JGT
+
+	(Continue)
+	// Add 1 to currentIndex and jump to the start of thirdLoop
+		@currentIndex
+		M=M+1
+		@thirdLoop
+		0;JMP
+	
+(Swap)
+// Swap array[currentIndex] with array[currentIndex + 1], add jump to continue
+	// Save array[currentIndex] in temporary
+	@currentIndex
+	A=M
+	D=M
+	@temporary
+	M=D
+	
+	//Save array[currentIndex+1] in array[currentIndex]
+	@currentIndex
+	A=M+1
+	D=M
+	@currentIndex
+	A=M
+	M=D
+	
+	// Save temporary in array[currentIndex+1]
+	@temporary
+	D=M
+	@currentIndex
+	A=M+1
+	M=D
+	
+	// Set isSwapped to 1
+	@isSwapped
+	M=1
+	
+	@Continue
+	0;JMP
+		
+(GoBackToSec)
+	// Add 1 to "startIndex" and go back to secondLoop, otherwise continue
+	@startIndex
+	M=M+1
+	@secondLoop
+	0;JMP
+
+(END)
+	@END
+	0;JMP
